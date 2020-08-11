ea78be59aecb42090e4d726178070711879277de
André Voronhuk <andrevoronhuk@gmail.com>
2020-08-10 21:47:42 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+console.log("------------\n\nSERVER UP!\n\n------------");
+import express from "express";
+import routes from "./routes";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
